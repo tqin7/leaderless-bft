f288a4a055deaa5bb357a7328a63b04d3ba44d37
Tian Qin <tianqin@berkeley.edu>
2019-04-25 01:00:00 -0700

    finished script to generate nodes ip with different ports for testing on Berkeley cluster

--- client/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null ; then
-    echo "running"
-else
-    echo "not running"
-fi
---
+++ tests/network/generate_nodes_same_ip.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+numOfNodes=4
+
+maxNeighborSize=$((numOfNodes / 2))
+ips=()
+nodesGenerated=0
+curPort=30000
+while [[ ${nodesGenerated} -lt ${numOfNodes} ]]
+do
+    if lsof -Pi :${curPort} -sTCP:LISTEN -t >/dev/null ; then
+        echo "port in use, skip."
+    else
+#        echo "generate node $curPort"
+        ips+=(127.0.0.1:$curPort)
+        nodesGenerated=$((nodesGenerated+1))
+    fi
+    curPort=$((curPort+1))
+done
+
+echo -e "\nnodes generated: ${ips[@]}"
+
+jsonStr=""
+
+for ((i=0; i<numOfNodes; i++)); do
+    numNeighbors=$((1 + RANDOM % maxNeighborSize))
+
+    peerStr="${ips[numOfNodes-1]}"
+    if (("$i" > 0))
+    then
+        peerStr="${ips[i-1]}"
+    fi
+
+    for ((j=0; j<numNeighbors; j++)); do
+        randomPeer=${ips[$RANDOM % numOfNodes]}
+        peerStr="${peerStr:+$peerStr,}$randomPeer"
+    done
+    peerObj="{\"ip\":\"${ips[i]}\", \"peers\":\"$peerStr\"}"
+    jsonStr="${jsonStr:+$jsonStr,}$peerObj"
+done
+jsonStr="{\"nodes\":[$jsonStr]}"
+
+echo ${jsonStr} > config.json
+
+echo -e "wrote network config to json file\n"
+
+# go to https://codebeautify.org/jsonviewer to beautify the json generated
